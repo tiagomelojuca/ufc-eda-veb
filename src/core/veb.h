8a1910e25100c0cbcd2c74971744bf3f05d9db03
Tiago Melo Juca <contact@tiagomelojuca.com>
2025-07-25 17:02:12 -0300

    fazer stub da veb

---
+++ src/core/veb.h
@@ -0,0 +1,53 @@
+#ifndef VEB_H_
+#define VEB_H_
+
+#include <cstdint>
+#include <string>
+
+#define _MAXINT 2147483647
+
+namespace ufc
+{
+namespace eda
+{
+namespace core
+{
+
+class veb
+{
+public:
+    using word_t = int32_t;
+    constexpr static const word_t inf = _MAXINT;
+
+    veb() = default;
+    ~veb() = default;
+
+    void inclui(word_t chave)
+    {
+    }
+
+    void remove(word_t chave)
+    {
+    }
+
+    word_t sucessor(word_t chave) const
+    {
+        return -1;
+    }
+
+    word_t predecessor(word_t chave) const
+    {
+        return -1;
+    }
+
+    std::string to_string() const
+    {
+        return "";
+    }
+};
+
+}
+}
+}
+
+#endif // VEB_H_
--- src/io/executor.h
+++ src/io/executor.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 
+#include "core/veb.h"
 #include "io/file_writer.h"
 #include "io/operacao.h"
 
@@ -40,32 +41,32 @@ private:
     {
         if (op.tipoOperacao == ufc::eda::io::op::tipo::INCLUSAO)
         {
-            // arvore.inclui(op.lparam);
+            arvore.inclui(op.param);
         }
         else if (op.tipoOperacao == ufc::eda::io::op::tipo::REMOCAO)
         {
-            // arvore.remove(op.lparam);
+            arvore.remove(op.param);
         }
         else if (op.tipoOperacao == ufc::eda::io::op::tipo::SUCESSAO)
         {
             std::string str_sucessor = "INF";
 
-            // const int sucessor = arvore.sucessor(op.lparam, op.rparam);
-            // if (sucessor != ufc::eda::persistencia::abb::inf)
-            // {
-            //     str_sucessor = std::to_string(sucessor);
-            // }
+            const int sucessor = arvore.sucessor(op.param);
+            if (sucessor != ufc::eda::core::veb::inf)
+            {
+                str_sucessor = std::to_string(sucessor);
+            }
 
             fwriter << op << str_sucessor << "\n";
         }
         else if (op.tipoOperacao == ufc::eda::io::op::tipo::IMPRESSAO)
         {
-            // fwriter << op << ufc::eda::io::utils::to_string(arvore, op.lparam) << "\n";
+            fwriter << op << arvore.to_string() << "\n";
         }
     }
 
     std::string arquivo_saida;
-    // ufc::eda::persistencia::abb arvore;
+    ufc::eda::core::veb arvore;
     std::vector<op> _operacoes;
 };
 
